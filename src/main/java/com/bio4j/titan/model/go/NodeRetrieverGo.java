cd6bcffb47da66b0f72e4cd16adeac5b1a51780e
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-05-14 10:51:01 +0200

    minor updates

--- src/main/java/com/bio4j/titan/model/go/NodeRetrieverGo.java
+++ src/main/java/com/bio4j/titan/model/go/NodeRetrieverGo.java
@@ -2,9 +2,15 @@ package com.bio4j.titan.model.go;
 
 import com.bio4j.model.go.relationships.Term;
 import com.bio4j.titan.model.go.nodes.GoTerm;
+import com.ohnosequences.typedGraphs.Retriever;
 
 public class NodeRetrieverGo extends Retriever{
 	
+	NodeRetrieverGo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public Term getTermById(String id){
 		getNodeFrom(arg0, arg1)
 	}
