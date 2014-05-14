80d2b20a95e09f904ede6d2e45989eeda8cd25a7
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-05-14 10:27:53 +0200

    minor updates

--- src/main/java/com/bio4j/titan/model/go/GoModule.java
+++ src/main/java/com/bio4j/titan/model/go/GoModule.java
@@ -1,4 +1,4 @@
-package com.bio4j.model.go;
+package com.bio4j.titan.model.go;
 
 import java.util.Set;
 import java.util.HashSet;
@@ -7,47 +7,76 @@ import com.ohnosequences.typedGraphs.Module;
 import com.ohnosequences.typedGraphs.NodeType;
 import com.ohnosequences.typedGraphs.RelationshipType;
 import com.ohnosequences.typedGraphs.PropertyType;
-
 import com.bio4j.model.go.nodes.*;
 import com.bio4j.model.properties.*;
+import com.bio4j.model.util.NodeIndex;
 import com.bio4j.model.go.relationships.*;
 
 public enum GoModule implements Module {
 
-  go;
-  
-  // package name
-  public static String PKG = "com.bio4j.model.go";
-  // dependencies
-  public static Set<Module> DEPENDENCIES = new HashSet<Module>();
-  // node types
-  public static Set<NodeType> NODE_TYPES = new HashSet<NodeType>() {{
-    add(GoTerm.TYPE);
-  }};
-  // relationship types
-  public static Set<RelationshipType> RELATIONSHIP_TYPES = new HashSet<RelationshipType>() {{
-    add(IsA.TYPE); 
-    add(PartOf.TYPE); 
-    add(HasPartOf.TYPE); 
-    add(NegativelyRegulates.TYPE); 
-    add(PositivelyRegulates.TYPE); 
-    add(Regulates.TYPE);
-  }};
-  // property types
-  public static Set<PropertyType> PROPERTY_TYPES = new HashSet<PropertyType>() {{
-    // GoTerm
-    add(Id.<GoTerm, GoTerm.Type>TYPE(GoTerm.TYPE)); 
-    add(Definition.<GoTerm, GoTerm.Type>TYPE(GoTerm.TYPE));
-    add(Comment.<GoTerm, GoTerm.Type>TYPE(GoTerm.TYPE));
-    add(Obsolete.<GoTerm, GoTerm.Type>TYPE(GoTerm.TYPE));
-    add(Name.<GoTerm, GoTerm.Type>TYPE(GoTerm.TYPE));
-    add(AlternativeIds.<GoTerm, GoTerm.Type>TYPE(GoTerm.TYPE));
-  }};
+	go;
 
+	// package name
+	public static String PKG = "com.bio4j.model.go";
+	// dependencies
+	public static Set<Module> DEPENDENCIES = new HashSet<Module>();
+	// node types
+	public static Set<NodeType> NODE_TYPES = new HashSet<NodeType>() {
+		{
+			add(GoTerm.TYPE);
+		}
+	};
+	// relationship types
+	public static Set<RelationshipType> RELATIONSHIP_TYPES = new HashSet<RelationshipType>() {
+		{
+			add(IsA.TYPE);
+			add(PartOf.TYPE);
+			add(HasPartOf.TYPE);
+			add(NegativelyRegulates.TYPE);
+			add(PositivelyRegulates.TYPE);
+			add(Regulates.TYPE);
+		}
+	};
+	// property types
+	public static Set<PropertyType> PROPERTY_TYPES = new HashSet<PropertyType>() {
+		{
+			// GoTerm
+			add(Id.<GoTerm, GoTerm.Type> TYPE(GoTerm.TYPE));
+			add(Definition.<GoTerm, GoTerm.Type> TYPE(GoTerm.TYPE));
+			add(Comment.<GoTerm, GoTerm.Type> TYPE(GoTerm.TYPE));
+			add(Obsolete.<GoTerm, GoTerm.Type> TYPE(GoTerm.TYPE));
+			add(Name.<GoTerm, GoTerm.Type> TYPE(GoTerm.TYPE));
+			add(AlternativeIds.<GoTerm, GoTerm.Type> TYPE(GoTerm.TYPE));
+		}
+	};
+	
+	// node indices
+	public static Set<NodeIndex> NODE_INDICES = new HashSet<NodeIndex>() {
+		{
+		}
+	};
 
-  public String pkg() { return PKG; }
-  public Set<Module> dependencies() { return DEPENDENCIES; }
-  public Set<PropertyType> propertyTypes() { return PROPERTY_TYPES; }
-  public Set<NodeType> nodeTypes() { return NODE_TYPES; }
-  public Set<RelationshipType> relationshipTypes() { return RELATIONSHIP_TYPES; }
+	public String pkg() {
+		return PKG;
+	}
+
+	public Set<Module> dependencies() {
+		return DEPENDENCIES;
+	}
+
+	public Set<PropertyType> propertyTypes() {
+		return PROPERTY_TYPES;
+	}
+
+	public Set<NodeType> nodeTypes() {
+		return NODE_TYPES;
+	}
+
+	public Set<RelationshipType> relationshipTypes() {
+		return RELATIONSHIP_TYPES;
+	}
+	
+	public Set<NodeIndex> nodeIndices(){
+		return NODE_INDICES;
+	}
 }
---
+++ src/main/java/com/bio4j/titan/model/go/NodeRetrieverGo.java
@@ -0,0 +1,12 @@
+package com.bio4j.titan.model.go;
+
+import com.bio4j.model.go.relationships.Term;
+import com.bio4j.titan.model.go.nodes.GoTerm;
+
+public class NodeRetrieverGo extends Retriever{
+	
+	public Term getTermById(String id){
+		getNodeFrom(arg0, arg1)
+	}
+
+}
