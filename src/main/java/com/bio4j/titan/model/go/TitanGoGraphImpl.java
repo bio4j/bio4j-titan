07a39e917d52b6cb2d2eb5ce416cc9c19433931a
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-06-03 12:08:29 +0200

    formatting some code

--- src/main/java/com/bio4j/titan/model/go/TitanGoGraphImpl.java
+++ src/main/java/com/bio4j/titan/model/go/TitanGoGraphImpl.java
@@ -4,19 +4,22 @@ import com.thinkaurelius.titan.core.*;
 
 public final class TitanGoGraphImpl extends TitanGoGraph {
 
-  TitanGoGraphImpl(TitanGraph graph) { super(graph); initTypes(); }
+    TitanGoGraphImpl(TitanGraph graph) {
+        super(graph);
+        initTypes();
+    }
 
-  private void initTypes() {
+    private void initTypes() {
 
 
-    // Term keys
-    termTkey = titanKeyForNodeType(termT.id);
-    termIdKey = termTkey;
-    termNameKey = titanKeyForNodeProperty(termT.name).make();
-    // if you want to index the name just add the corresponding titan stuff before make, like
-    // termNameKey = titanKeyForNodeProperty(termT.name).indexed(com.tinkerpop.blueprints.Edge.class).make();
+        // Term keyss
+        termTkey = titanKeyForNodeType(termT.id);
+        termIdKey = termTkey;
+        termNameKey = titanKeyForNodeProperty(termT.name).make();
+        // if you want to index the name just add the corresponding titan stuff before make, like
+        // termNameKey = titanKeyForNodeProperty(termT.name).indexed(com.tinkerpop.blueprints.Edge.class).make();
 
-    // partOf stuff
-    partOfLabel = titanLabelForRelationshipType(partOfT).make();
-  }
+        // partOf stuff
+        partOfLabel = titanLabelForRelationshipType(partOfT).make();
+    }
 }
