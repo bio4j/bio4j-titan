c15d1f0332fe08e475fcde1a3e2325121508249e
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-07-23 12:10:01 +0200

    some updates in taxonomy graph classes

--- src/main/java/com/bio4j/titan/model/ncbiTaxonomy/TitanNCBITaxonomyGraphImpl.java
+++ src/main/java/com/bio4j/titan/model/ncbiTaxonomy/TitanNCBITaxonomyGraphImpl.java
@@ -1,7 +1,33 @@
 package com.bio4j.titan.model.ncbiTaxonomy;
 
+import com.ohnosequences.typedGraphs.titan.TitanNodeIndex;
+import com.thinkaurelius.titan.core.TitanGraph;
+
 /**
  * Created by ppareja on 7/14/2014.
  */
-public class TitanNCBITaxonomyGraphImpl {
+public class TitanNCBITaxonomyGraphImpl extends  TitanNCBITaxonomyGraph{
+
+	public TitanNCBITaxonomyGraphImpl(TitanGraph graph) {
+		super(graph);
+		initTypes();
+		initIndices();
+	}
+
+
+	private void initTypes() {
+
+		// NCBITaxon keys
+		nCBITaxonTkey = titanKeyForNodeType(nCBITaxonT.id);
+		nCBITaxonIdKey = nCBITaxonTkey;
+		nCBITaxonNameKey = titanKeyForNodeProperty(nCBITaxonT.name);
+		nCBITaxonScientificNameKey = titanKeyForNodeProperty(nCBITaxonT.scientificName);
+		nCBITaxonTaxonomicRankKey = titanKeyForNodeProperty(nCBITaxonT.taxonomicRank);
+		nCBITaxonCommentKey = titanKeyForNodeProperty(nCBITaxonT.comment);
+
+	}
+
+	private void initIndices() {
+		nCBITaxonIdIndex = new TitanNodeIndex.DefaultUnique(this, nCBITaxonT.id);
+	}
 }
