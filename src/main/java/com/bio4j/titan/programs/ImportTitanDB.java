4ae786baacf96ec676dbf6693a821d477ed5667e
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-08-28 18:30:30 +0200

    adding class to import DB

---
+++ src/main/java/com/bio4j/titan/programs/ImportTitanDB.java
@@ -0,0 +1,13 @@
+package com.bio4j.titan.programs;
+
+import com.ohnosequences.util.ExecuteFromFile;
+
+/**
+ * Created by raquel on 28/08/14.
+ */
+public class ImportTitanDB {
+
+    public static void main(String args[]){
+        ExecuteFromFile.main(args);
+    }
+}
