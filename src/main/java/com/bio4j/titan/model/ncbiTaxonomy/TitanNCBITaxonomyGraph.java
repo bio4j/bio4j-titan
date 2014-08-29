7ac864ee070ebf2933adcd6635f24638ad360874
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-08-29 16:08:52 +0200

    a few fixes

--- src/main/java/com/bio4j/titan/model/ncbiTaxonomy/TitanNCBITaxonomyGraph.java
+++ src/main/java/com/bio4j/titan/model/ncbiTaxonomy/TitanNCBITaxonomyGraph.java
@@ -1,7 +1,75 @@
 package com.bio4j.titan.model.ncbiTaxonomy;
 
+import com.bio4j.model.ncbiTaxonomy.NCBITaxonomyGraph;
+import com.bio4j.model.ncbiTaxonomy.nodes.NCBITaxon;
+import com.bio4j.titan.util.DefaultTitanGraph;
+import com.ohnosequences.typedGraphs.titan.TitanTypedVertexIndex;
+import com.thinkaurelius.titan.core.*;
+
+
 /**
- * Created by raquel on 27/08/14.
+ Implementing the types with Titan
+ @author <a href="mailto:ppareja@era7.com">Pablo Pareja Tobes</a>
  */
-public class TitanNCBITaxonomyGraph {
+public final class TitanNCBITaxonomyGraph
+        extends
+        NCBITaxonomyGraph<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel> {
+
+    private DefaultTitanGraph rawGraph;
+
+
+    //-------------------VERTICES----------------------------
+
+    public TitanKey nCBITaxonTypekey;
+    public TitanKey nCBITaxonIdkey;
+    public NCBITaxonType nCBITaxonType;
+
+
+    //---------------INDICES---------------------------
+
+    TitanTypedVertexIndex.DefaultUnique<
+            NCBITaxon<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel>,
+            NCBITaxonType,
+            NCBITaxonType.id, String,
+            NCBITaxonomyGraph<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel>,
+            DefaultTitanGraph
+            > nCBITaxonIdIndex;
+
+    public TitanNCBITaxonomyGraph(DefaultTitanGraph rawGraph) {
+        super(rawGraph);
+        this.rawGraph = rawGraph;
+        initTypes();
+        initIndices();
+    }
+
+    @Override
+    public DefaultTitanGraph raw() {
+        return rawGraph;
+    }
+
+    private void initTypes() {
+
+        //-----------------------------------------------------------------------------------------
+        //--------------------------------VERTICES--------------------------------------------
+        nCBITaxonTypekey = raw().titanKeyMakerForVertexType(NCBITaxon().id).single().unique().make();
+        nCBITaxonIdkey = nCBITaxonTypekey;
+        nCBITaxonType = new NCBITaxonType(nCBITaxonTypekey);
+
+
+    }
+
+    private void initIndices() {
+        nCBITaxonIdIndex =  new TitanTypedVertexIndex.DefaultUnique(this, NCBITaxon().id);
+    }
+
+
+    @Override
+    public NCBITaxonType NCBITaxon() {
+        return nCBITaxonType;
+    }
+
+    @Override
+    public NCBITaxonParentType NCBITaxonParent() {
+        return null;
+    }
 }
--- src/main/java/com/bio4j/titan/model/uniprot_enzyme/TitanUniprotEnzymeGraph.java
+++ src/main/java/com/bio4j/titan/model/uniprot_enzyme/TitanUniprotEnzymeGraph.java
@@ -1,7 +1,59 @@
 package com.bio4j.titan.model.uniprot_enzyme;
 
+import com.bio4j.model.enzymedb.EnzymeDBGraph;
+import com.bio4j.model.uniprot.UniprotGraph;
+import com.bio4j.model.uniprot_enzymedb.UniprotEnzymeDBGraph;
+import com.bio4j.titan.util.DefaultTitanGraph;
+import com.ohnosequences.typedGraphs.titan.TitanTypedVertexIndex;
+import com.thinkaurelius.titan.core.*;
+
+
 /**
- * Created by raquel on 27/08/14.
+ Implementing the types with Titan
+ @author <a href="mailto:ppareja@era7.com">Pablo Pareja Tobes</a>
  */
-public class TitanUniprotEnzymeGraph {
+public final class TitanUniprotEnzymeGraph
+        extends
+        UniprotEnzymeDBGraph<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel> {
+
+    private DefaultTitanGraph rawGraph;
+
+
+
+//    public TitanUniprotEnzymeGraph(DefaultTitanGraph rawGraph) {
+//        super(rawGraph);
+//        this.rawGraph = rawGraph;
+//        initTypes();
+//        initIndices();
+//    }
+
+    @Override
+    public DefaultTitanGraph raw() {
+        return rawGraph;
+    }
+
+    private void initTypes() {
+
+
+    }
+
+    private void initIndices() {
+
+    }
+
+
+    @Override
+    public UniprotGraph<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel> uniprotGraph() {
+        return null;
+    }
+
+    @Override
+    public EnzymeDBGraph<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel> enzymeDBGraph() {
+        return null;
+    }
+
+    @Override
+    public EnzymaticActivityType EnzymaticActivity() {
+        return null;
+    }
 }
