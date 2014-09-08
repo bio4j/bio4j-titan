f5386d2e9d9a716c685deea4c74b1fbdf58043be
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-09-08 16:51:06 +0200

    some updates at the test for indices...

--- src/test/java/com/bio4j/titan/tests/IndexTest.java
+++ src/test/java/com/bio4j/titan/tests/IndexTest.java
@@ -1,5 +1,6 @@
 package com.bio4j.titan.tests;
 
+import com.bio4j.model.uniprot.nodes.EMBL;
 import com.bio4j.model.uniprot.nodes.RefSeq;
 import com.bio4j.titan.model.uniprot.TitanUniprotGraph;
 import com.bio4j.titan.util.DefaultTitanGraph;
@@ -27,14 +28,27 @@ public class IndexTest {
 		String idSt = "NP_505817.1";
 
 		RefSeq<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel> refSeq = null;
+		EMBL<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel> embl = null;
+
 
 		refSeq = graph.RefSeq().from(graph.raw().addVertex(null));
 		refSeq.set(graph.RefSeq().id, idSt);
 		graph.raw().commit();
 
+		embl = graph.EMBL().from(graph.raw().addVertex(null));
+		embl.set(graph.EMBL().id, idSt);
+		graph.raw().commit();
+
 		Optional<RefSeq<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel>> optionalRefSeq = graph.refSeqIdIndex().getVertex(idSt);
 		if(!optionalRefSeq.isPresent()){
-			System.out.println("ERROR: The id was not found... :(");
+			System.out.println("ERROR: The REfSEq id was not found... :(");
+		}else{
+			System.out.println("Element found! :)");
+		}
+
+		Optional<EMBL<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel>> optionalEMBL = graph.eMBLIdIndex().getVertex(idSt);
+		if(!optionalEMBL.isPresent()){
+			System.out.println("ERROR: The EMBL id was not found... :(");
 		}else{
 			System.out.println("Element found! :)");
 		}
