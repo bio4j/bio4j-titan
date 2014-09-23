72eed4a83ab8665d4e2e97ad0d6c7775b5480f61
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-09-23 10:04:53 +0200

    adding class ImportIsoformSequencesTitan

---
+++ src/main/java/com/bio4j/titan/model/uniprot/programs/ImportIsoformSequencesTitan.java
@@ -0,0 +1,59 @@
+/*
+  * Copyright (C) 2010-2013  "Bio4j"
+  *
+  * This file is part of Bio4j
+  *
+  * Bio4j is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Affero General Public License as
+  * published by the Free Software Foundation, either version 3 of the
+  * License, or (at your option) any later version.
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Affero General Public License for more details.
+  * You should have received a copy of the GNU Affero General Public License
+  * along with this program.  If not, see <http:www.gnu.org/licenses/>
+  */
+package com.bio4j.titan.model.uniprot.programs;
+
+import com.bio4j.model.uniprot.UniprotGraph;
+import com.bio4j.model.uniprot.programs.ImportIsoformSequences;
+import com.bio4j.titan.model.uniprot.TitanUniprotGraph;
+import com.bio4j.titan.util.DefaultTitanGraph;
+import com.ohnosequences.util.Executable;
+import com.thinkaurelius.titan.core.*;
+import org.apache.commons.configuration.BaseConfiguration;
+import org.apache.commons.configuration.Configuration;
+
+
+import java.util.ArrayList;
+
+/**
+ * Imports Uniprot into Bio4j
+ *
+ * @author <a href="mailto:ppareja@era7.com">Pablo Pareja Tobes</a>
+ */
+public class ImportIsoformSequencesTitan extends ImportIsoformSequences<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel> implements Executable {
+
+	@Override
+	protected UniprotGraph<DefaultTitanGraph, TitanVertex, TitanKey, TitanEdge, TitanLabel> config(String dbFolder) {
+		//----------DB configuration------------------
+		Configuration conf = new BaseConfiguration();
+		conf.setProperty("storage.directory", dbFolder);
+		conf.setProperty("storage.backend", "local");
+		conf.setProperty("autotype", "none");
+		//-------creating graph handlers---------------------
+		TitanGraph graph = TitanFactory.open(conf);
+		return new TitanUniprotGraph(new DefaultTitanGraph(graph));
+	}
+
+	@Override
+	public void execute(ArrayList<String> array) {
+		String[] args = new String[array.size()];
+		for (int i = 0; i < array.size(); i++) {
+			args[i] = array.get(i);
+		}
+		importIsoformSequences(args);
+	}
+
+}
