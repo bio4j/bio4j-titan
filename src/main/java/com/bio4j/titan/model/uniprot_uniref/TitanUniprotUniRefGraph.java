17ec2016d1ba630a1e6d1212bb79c78647fb2087
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-11-17 12:19:03 +0100

    adding initialization of proteinAccession property in UniprotUniRef graph

--- src/main/java/com/bio4j/titan/model/uniprot_uniref/TitanUniprotUniRefGraph.java
+++ src/main/java/com/bio4j/titan/model/uniprot_uniref/TitanUniprotUniRefGraph.java
@@ -25,16 +25,27 @@ public final class TitanUniprotUniRefGraph
 
     private TitanLabel uniRef50MemberLabel;
     private UniRef50MemberType uniRef50MemberType;
+	public TitanKey uniRef50MemberProteinAccessionKey;
+
     private TitanLabel uniRef90MemberLabel;
     private UniRef90MemberType uniRef90MemberType;
+	public TitanKey uniRef90MemberProteinAccessionKey;
+
     private TitanLabel uniRef100MemberLabel;
     private UniRef100MemberType uniRef100MemberType;
+	public TitanKey uniRef100MemberProteinAccessionKey;
+
     private TitanLabel uniRef50RepresentantLabel;
     private UniRef50RepresentantType uniRef50RepresentantType;
+	public TitanKey uniRef50RepresentantProteinAccessionKey;
+
     private TitanLabel uniRef90RepresentantLabel;
     private UniRef90RepresentantType uniRef90RepresentantType;
+	public TitanKey uniRef90RepresentantProteinAccessionKey;
+
     private TitanLabel uniRef100RepresentantLabel;
     private UniRef100RepresentantType uniRef100RepresentantType;
+	public TitanKey uniRef100RepresentantProteinAccessionKey;
 
 
     public TitanUniprotUniRefGraph(DefaultTitanGraph rawGraph, TitanUniprotGraph titanUniprotGraph, TitanUniRefGraph titanUniRefGraph) {
@@ -58,18 +69,28 @@ public final class TitanUniprotUniRefGraph
 
         uniRef50MemberLabel = raw().titanLabelForEdgeType(new UniRef50MemberType((TitanLabel) null));
         uniRef50MemberType = new UniRef50MemberType(uniRef50MemberLabel);
+	    uniRef50MemberProteinAccessionKey = raw().titanKeyForEdgePropertySingle(uniRef50MemberType.proteinAccession);
+
         uniRef90MemberLabel = raw().titanLabelForEdgeType(new UniRef90MemberType((TitanLabel) null));
         uniRef90MemberType = new UniRef90MemberType(uniRef90MemberLabel);
+	    uniRef90MemberProteinAccessionKey = raw().titanKeyForEdgePropertySingle(uniRef90MemberType.proteinAccession);
+
         uniRef100MemberLabel = raw().titanLabelForEdgeType(new UniRef100MemberType((TitanLabel) null));
         uniRef100MemberType = new UniRef100MemberType(uniRef100MemberLabel);
-
+	    uniRef100MemberProteinAccessionKey = raw().titanKeyForEdgePropertySingle(uniRef100MemberType.proteinAccession);
 
         uniRef50RepresentantLabel = raw().titanLabelForEdgeType(new UniRef50RepresentantType((TitanLabel) null));
         uniRef50RepresentantType = new UniRef50RepresentantType(uniRef50RepresentantLabel);
+	    uniRef50RepresentantProteinAccessionKey = raw().titanKeyForEdgePropertySingle(uniRef50RepresentantType.proteinAccession);
+
         uniRef90RepresentantLabel = raw().titanLabelForEdgeType(new UniRef90RepresentantType((TitanLabel) null));
         uniRef90RepresentantType = new UniRef90RepresentantType(uniRef90RepresentantLabel);
-        uniRef100RepresentantLabel = raw().titanLabelForEdgeType(new UniRef100RepresentantType((TitanLabel) null));
+	    uniRef90RepresentantProteinAccessionKey = raw().titanKeyForEdgePropertySingle(uniRef90RepresentantType.proteinAccession);
+
+	    uniRef100RepresentantLabel = raw().titanLabelForEdgeType(new UniRef100RepresentantType((TitanLabel) null));
         uniRef100RepresentantType = new UniRef100RepresentantType(uniRef100RepresentantLabel);
+	    uniRef100RepresentantProteinAccessionKey = raw().titanKeyForEdgePropertySingle(uniRef100RepresentantType.proteinAccession);
+
 
     }
 
