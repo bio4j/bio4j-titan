69335791edd7935d38c4173b96f7af1a581d9d59
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-12-15 12:15:09 +0100

    adding program to split UniProt XML file in smaller chunks so that things can later fit in memory

---
+++ src/main/java/com/bio4j/titan/model/uniprot/programs/SplitUniProtXMLFile.java
@@ -0,0 +1,90 @@
+package com.bio4j.titan.model.uniprot.programs;
+
+import com.era7.bioinfo.bioinfoutil.Executable;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Pablo Pareja Tobes <ppareja@era7.com>
+ */
+public class SplitUniProtXMLFile implements Executable{
+
+	public static final String ENTRY_TAG_NAME = "entry";
+
+	@Override
+	public void execute(ArrayList<String> array) {
+		String[] args = new String[array.size()];
+		for (int i = 0; i < array.size(); i++) {
+			args[i] = array.get(i);
+		}
+		main(args);
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("This program expects two parameters: \n"
+					+ "1. UniProt XML input file\n"
+					+ "2. Number of entries per resulting part file\n");
+		} else {
+
+			int numberOfEntries = Integer.parseInt(args[1]);
+			int currentFile = 1;
+			int currentEntry = 1;
+
+			File inFile = new File(args[0]);
+
+			String prefixOutFile = args[0].split("\\.")[0];
+
+			try {
+
+				BufferedWriter outBuff = new BufferedWriter(new FileWriter(new File(prefixOutFile + currentFile + ".xml")));
+				outBuff.write("<entries>\n");
+
+				BufferedReader reader = new BufferedReader(new FileReader(inFile));
+				String line = null;
+
+				while ((line = reader.readLine()) != null) {
+					if (line.trim().startsWith("<" + ENTRY_TAG_NAME)) {
+
+						if(currentEntry % numberOfEntries == 0){
+							outBuff.write("</entries>\n");
+							outBuff.close();
+							System.out.println("Closing file...");
+							currentFile++;
+							outBuff = new BufferedWriter(new FileWriter(new File(prefixOutFile + currentFile + ".xml")));
+							outBuff.write("<entries>\n");
+						}
+
+						outBuff.write((line + "\n"));
+
+						while (!line.trim().startsWith("</" + ENTRY_TAG_NAME + ">")) {
+							line = reader.readLine();
+							outBuff.write((line + "\n"));
+						}
+
+						outBuff.flush();
+
+						currentEntry++;
+
+						if(currentEntry % 10000 == 0){
+							System.out.println(currentEntry + " already...");
+						}
+					}
+				}
+
+				outBuff.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+
+		}
+	}
+}
